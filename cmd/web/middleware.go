b1455873a243e0b154cb909f65903548c0776a27
mick <vashinred@gmail.com>
2025-08-19 12:12:57 -0700

    origin was spelled wrong the whole time

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -10,9 +10,9 @@ import (
 
 func secureHeaders(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Security-Policy", "default-src 'self'; stype-src 'self' fonts.googleapis.com; font-src fonts.gstatic.com")
+		w.Header().Set("Content-Security-Policy", "default-src 'self'; style-src 'self' fonts.googleapis.com; font-src fonts.gstatic.com")
 
-		w.Header().Set("Referrer-Policy", "orgin-when-cross-orgin")
+		w.Header().Set("Referrer-Policy", "origin-when-cross-origin")
 		w.Header().Set("X-Content-Type-Options", "nosniff")
 		w.Header().Set("X-Frame-Options", "deny")
 		w.Header().Set("X-XSS-Protection", "0")
